02b2467828c234d631b871819a5d68180d657898
fox <foxiswho@qq.com>
2021-04-06 13:25:08 +0800

    二级目录可预览

---
+++ assets/ProjectDetail.45f7731c.js
@@ -0,0 +1,2 @@
+import{a as e}from"./index.04508799.js";import{d as a,e as l,r as n,o,c as t,m,x as p}from"./element-plus.93d71f21.js";var r=a({name:"ProjectDetail",setup:()=>({projName:e().params.projName,input:l("")})});const s={id:"dynamicexample"},u=m("h2",null,"项目详情",-1);r.render=function(e,a,l,r,d,i){const c=n("el-input");return o(),t("div",s,[u,m("p",null,"项目编码："+p(e.projName),1),m(c,{modelValue:e.input,"onUpdate:modelValue":a[1]||(a[1]=a=>e.input=a)},null,8,["modelValue"])])};export default r;
+//# sourceMappingURL=ProjectDetail.45f7731c.js.map
